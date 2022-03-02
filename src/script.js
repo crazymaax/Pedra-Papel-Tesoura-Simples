30ecebd5ef1c7e4a461fa1cb505953321a8adb10
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-03-02 20:19:35 -0300

    Script para iniciar o jogo.

---
+++ src/script.js
@@ -0,0 +1,8 @@
+import { Game } from "../src/models/Game.js";
+
+const newGame = new Game();
+newGame.playersClick()
+
+/* https://www.figma.com/file/G5SGm1KowaW4PaClNc5t2P/Pedra-Papel-Tesoura?node-id=0%3A1 */
+
+
